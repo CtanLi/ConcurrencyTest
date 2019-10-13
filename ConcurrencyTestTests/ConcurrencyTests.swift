99c831ba6ec582cc224e87a0ea19952c5962aacb
Stanley <adigwestanley@gmail.com>
2019-10-13 13:56:55 -0400

    unit test implemented for successful message loading.

--- ConcurrencyTestTests/ConcurrencyTests.swift
+++ ConcurrencyTestTests/ConcurrencyTests.swift
@@ -10,6 +10,24 @@ import XCTest
 class ConcurrencyTests: XCTestCase {
     func testloadMessage() {
         
+        let fetchMessage = FetchMessages()
+        
+        //Defined expectation
+        let expec = expectation(description: "loadMessage executes and runs the callback closure")
+        
+        fetchMessage.loadMessage { (message, executionTime)  in
+    
+        executionTime < 2.0 ? XCTAssertEqual(message, "Good morning!") : XCTAssertEqual(message, "Unable to load message - Time out exceeded")
+        
+            //fulfill the expectation.
+            expec.fulfill()
+        }
+        
+        //Execution timeout behaviour
+        self.waitForExpectations(timeout: 5) { error in
+            if let error = error {
+                XCTFail("waitForExpectationsWithTimeout error: \(error)")
+            }
+        }
     }
-
 }
